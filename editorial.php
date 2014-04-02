521e3e8ac8565e410fd3f6df13d728d06119612c
emilien-puget <puget.e@gmail.com>
2014-04-02 11:11:22 +0200

    // cast return of install to bool

--- editorial.php
+++ editorial.php
@@ -33,7 +33,7 @@ class Editorial extends Module
 	{
 		$this->name = 'editorial';
 		$this->tab = 'front_office_features';
-		$this->version = '2.3';
+		$this->version = '2.4';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 		$this->bootstrap = true;
@@ -83,7 +83,7 @@ class Editorial extends Module
 		if (!$res)
 			$res &= $this->uninstall();
 
-		return $res;
+		return (bool)$res;
 	}
 
 	private function createExampleEditorial($id_shop)
